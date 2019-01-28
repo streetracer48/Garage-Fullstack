513b0b5c4a96454afb543919246cf324af36b068
miraz <shafi.spacesoft@gmail.com>
2019-01-28 12:09:12 +0600

    DB config connection added

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev')
+mongoose.connect(config.DB_URL)
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(3001, function() {
+    console.log('our app is runing port 3001');
+})
