46a0b69fe1da4210e67d9d3bf296748ee0834a40
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-01-31 00:33:34 +0600

    register with validation

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,45 @@
+const User = require('../models/user')
+
+
 exports.auth = function(req, res) {
 
     
 }
 
-exports.register = function (req, res) {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const passwordConfirmation = req.body.passwordConfirmation;
 
-    res.json({username, email});
-}
+exports.register =  function(req, res) {
+    const { username, email, password, passwordConfirmation } = req.body;
+  
+    if (!password || !email) {
+      return res.status(422).send({errors: [{title: 'Data missing!', detail: 'Provide email and password!'}]});
+    }
+  
+    if (password !== passwordConfirmation) {
+      return res.status(422).send({errors: [{title: 'Invalid passsword!', detail: 'Password is not a same as confirmation!'}]});
+    }
+  
+    User.findOne({email}, function(err, existingUser) {
+      if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
+      }
+  
+      if (existingUser) {
+        return res.status(422).send({errors: [{title: 'Invalid email!', detail: 'User with this email already exist!'}]});
+      }
+  
+      const user = new User({
+        username,
+        email,
+        password
+      });
+  
+      user.save(function(err) {
+        if (err) {
+          return res.status(422).send({'registered': true});
+        }
+  
+        return res.json({'registered': true});
+      })
+    })
+  }
+  
