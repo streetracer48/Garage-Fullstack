788d28c233a023ab912dfc34173ff34831dd8193
miraz <shafi.spacesoft@gmail.com>
2019-01-31 10:43:44 +0600

    password matching

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,35 @@ const MongooseErrorHelper = require('../helper/mongooseError')
 
 
 exports.auth = function(req, res) {
+  const {email, password} = req.body;
+
+  if(!password || !email) {
+    return res.status(422).send({errors: [{title: 'Data missing!', detail: 'Provide email and password!'}]});
+  }
+
+  User.findOne({email}, function(err, user) {
+     if(err) {
+      return res.status(422).send({errors:MongooseErrorHelper.normalizeErrors(err.errors)});
+     }
+
+     if(!user) {
+      return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'User does not exist!'}]});
+
+     }
+
+     if(user.isSamePassword(password))
+     {
+
+      //  return JWT token
+
+     }
+     else {
+      return res.status(422).send({errors: [{title: 'Wrong data!', detail: 'Wrong email or password!'}]});
+     }
+
+     
+  })
+
 
     
 }
