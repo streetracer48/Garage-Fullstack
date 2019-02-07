dc373173cdd8d3d9c7976d215a2704dc773feaf0
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-02-07 19:14:41 +0600

    header styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Component/shared/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header/>
       <h1>Hello react app</h1>
       </div>
     );
