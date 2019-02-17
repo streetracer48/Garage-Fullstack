7e6319497e5ae06d20f996699096be7ac1a2df07
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-02-17 12:42:19 +0600

    register

--- client/src/Component/login/index.jsx
+++ client/src/Component/login/index.jsx
@@ -12,18 +12,20 @@ class Login extends Component {
 
 
        render () {
+        //    console.log(this.props.location.state);
+         const {successRegister}  = this.props.location.state || false
             return ( 
                 <section id="login">
         <div className="bwm-form">
           <div className="row">
             <div className="col-md-5">
               <h1>Login</h1>
-              {/* {
+              {
                 successRegister &&
                   <div className='alert alert-success'>
                     <p> You have been succesfuly registered, please login now. </p>
                   </div>
-              } */}
+              }
               <LoginForm submitCb={this.loginUser}/>
             </div>
             <div className="col-md-6 ml-auto">
