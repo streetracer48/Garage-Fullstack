621e3867cf65dd80c8410c51515dbd57713d6e55
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-02-22 15:46:17 +0600

    disable booking date

--- client/src/Component/rental/booking/booking.jsx
+++ client/src/Component/rental/booking/booking.jsx
@@ -1,22 +1,54 @@
 import React,{Component} from 'react';
 import DateRangePicker from 'react-bootstrap-daterangepicker';
 import {getRangeOfDates} from '../../../helpers/index'
- class Booking extends Component {
+import * as moment from 'moment'
+
+class Booking extends Component {
+
+  
+    bookedOutDates=[]
+  
+    componentDidMount() {
+        this.getBookedOutDate()
+    }
 
 
+    getBookedOutDate = () => {
+        const {bookings} = this.props.rental;
+
+        if(bookings && bookings.length>0)
+        {
+
+            bookings.map(booking => {
+                const bookedDatesRange = getRangeOfDates(booking.startAt, booking.endAt, 'Y/MM/DD');
+              
+                this.bookedOutDates.push(...bookedDatesRange);
+             })
+             
+        }
+    // console.log(this.bookedOutDates);
+      
+     }
+
+     checkInvalidDates = (date) => {
+      return  this.bookedOutDates.includes(date.format('Y/MM/DD')) || date.diff(moment(), 'days') < 0;
+      }
+
+      
 
   render() {
     const {rental}= this.props
-
     return (
       <div className='booking'>
         <h3 className='booking-price'>$ {rental.dailyRate} <span className='booking-per-night'>per night</span></h3>
         <hr></hr>
         <div className='form-group'>
         <label htmlFor='dates'>Dates</label>
-      <DateRangePicker 
+                      <DateRangePicker 
                              opens='left'
-                             containerStyles={{display: 'block'}}>
+                             containerStyles={{display: 'block'}}
+                             isInvalidDate={this.checkInvalidDates}
+                             >
               <input  id='dates' type='text' className='form-control'></input>
             </DateRangePicker>
         </div>
