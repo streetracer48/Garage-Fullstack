9a6347fd895561c1075da28775c1b51a18a01276
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-03-19 00:42:54 +0600

    end point create for rental update patch

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -188,6 +188,35 @@ router.delete("/:id", UserCtr.authMiddleware, function(req, res){
 })
 
 
+router.patch('/:id', UserCtr.authMiddleware, function(req, res){
+      const rentalData = req.body;
+      const user = res.locals.user;
+    
+      Rental
+        .findById(req.params.id)
+        .populate('user')
+        .exec(function(err, foundRental) {
+    
+          if (err) {
+            return res.status(422).send({errors: normalizeErrors(err.errors)});
+          }
+    
+          if (foundRental.user.id !== user.id) {
+            return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'You are not rental owner!'}]});
+          }
+    
+          foundRental.set(rentalData);
+          foundRental.save(function(err) {
+            if (err) {
+              return res.status(422).send({errors: normalizeErrors(err.errors)});
+            }
+    
+            return res.status(200).send(foundRental);
+          });
+        }); 
+});
+
+
 
 
 
