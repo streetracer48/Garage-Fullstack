f5f70dd10f230945e835ab09d015c76ccbdb777e
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-05-09 02:28:08 +0600

    style added

---
+++ client/src/Component/payment/CheckoutForm.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import { injectStripe, CardElement } from 'react-stripe-elements';
+
+class CheckoutForm extends Component {
+
+
+}
+
+export default CheckoutForm
