9d501726193cf3398251d468ec7d6196ff574b6f
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-05-11 00:49:29 +0600

    confirm  function write

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
     DB_URL:'mongodb://ironman48:ironman48@ds245234.mlab.com:45234/booking',
     // DB_URL:'mongodb://localhost:27017/booking',
-    SECRET:'DSAFSDA3254'
+    SECRET:'DSAFSDA3254',
+    STRIPE_SK:'sk_test_TeANZ1I4Zrvzv7zxDQ2Mij1A00zNueQTNL'
 }
